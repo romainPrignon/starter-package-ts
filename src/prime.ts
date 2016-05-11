8ec9ab3e1a26303f05f00c1ffb907d8453116e61
Romain Prignon <rprignon@m6web.fr>
2016-05-11 14:21:45 +0200

    feat: require itself for release testing purpose

--- src/prime.ts
+++ src/prime.ts
@@ -1,4 +1,5 @@
 import 'ts-helpers'
+import PrimeFromStarter from '../node_modules/starter-package-ts/src/prime'
 
 export interface PrimeInterface {
   isPrime(n: number): boolean
@@ -33,6 +34,16 @@ export class PrimeService implements PrimeInterface {
 export class PrimeServiceExtended extends PrimeService implements PrimeInterfaceExtended {
   constructor(divisor: number = 2) {
     super(divisor)
+  }
+
+  /**
+   * Inception: use the isPrime function from self required package
+   * @param {Number} n
+   *
+   * @return {Boolean}
+   */
+  public isPrimeInception(n: number): boolean {
+    return new PrimeFromStarter().isPrime(n);
   }
 
   public primeFactors(n: number): Array<number> {
