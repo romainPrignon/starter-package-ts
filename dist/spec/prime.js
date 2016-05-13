3738e802d3479bdbf1bf691ffe47a479b6bdd3ff
Romain Prignon <rprignon@m6web.fr>
2016-05-13 11:32:20 +0200

    feat: prepare dist for release

---
+++ dist/spec/prime.js
@@ -0,0 +1,17 @@
+"use strict";
+var prime_1 = require('../src/prime');
+describe('(Service) Prime', function () {
+    describe('(Method) isPrime', function () {
+        var prime = new prime_1.default();
+        it('2 should be prime', function () {
+            expect(prime.isPrime(2)).toBe(true);
+        });
+    });
+    describe('(Method) isPrimeInception', function () {
+        var prime = new prime_1.default();
+        it('2 should be prime', function () {
+            expect(prime.isPrimeInception(2)).toBe(true);
+        });
+    });
+});
+//# sourceMappingURL=prime.js.map
