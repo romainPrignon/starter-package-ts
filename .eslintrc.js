4d6b1e9ae865ee253873189a69f1359c537ed2d8
romainprignon <pro.rprignon@gmail.com>
2019-12-20 14:43:15 +1300

    :recycle: add eslint typescript standard

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  env: {
+    es2020: true,
+    node: true, // EDIT_ME
+    browser: true, // EDIT_ME
+  },
+  extends: [
+    'standard',
+  ],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error'
+  }
+}
