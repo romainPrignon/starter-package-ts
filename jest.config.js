e553146fa4695ef0fe6d5b015e8ac07ff10c5297
romainprignon <pro.rprignon@gmail.com>
2020-01-19 15:19:40 +1300

    🔧 better linter and compiler workflow

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: [
-    '**/src/**/*.test.ts',
+    '**/test/**/*.test.ts',
     '**/spec/**/*.spec.ts',
   ],
   coverageThreshold: {
